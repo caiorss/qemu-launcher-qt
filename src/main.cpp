bc7b6799479defd7bd8889146707e75b8c3faacd
CaioRS <caiorss.rodrigues@gmail.com>
2021-08-17 16:15:25 -0300

    Implement functionality that allows only single app instance at a time.

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,26 @@ constexpr const char* APP_NAME = "qqemu";
 
 int main(int argc, char** argv)
 {
+
+    QSharedMemory shmem;
+    shmem.setKey("qemu-launcher-key");
+
     std::cout << " [INFO] Starting Application" << std::endl;
 
     QApplication app(argc, argv);
     app.setApplicationName(APP_NAME);   
+
+    if( !shmem.create(1) )
+    {
+        QMessageBox::warning( nullptr
+                             ,"Error report"
+                             ,"Only one application instance is allowed.");
+
+        std::cerr << " [ERROR] Only one application instance is allowed." << std::endl;
+        return -1;
+    }
+
+
 
     AppMainWindow maingui;
     maingui.setWindowIcon(QIcon(":/assets/appicon.png"));
